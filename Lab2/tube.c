6d5058e1f35fa277f59d5e537902c69c8ec00ab0
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-03 22:45:53 -0700

    Update tube.c

--- Lab2/tube.c
+++ Lab2/tube.c
@@ -4,8 +4,25 @@
 #include <sys/types.h>
 #include <string.h>
 
-int main()
+#define WRITE 1
+#define READ 0
+
+int main(int argc, char *argv[])
 {
   pid_t child1, child2;
+  int status1, status2;
+  int pipe[2];
+  
+  child1 = fork();
+  child2 = fork();
+  
+  pipe(pipe);
+  
+  waitpid(child1, &status1, WUNTRACED);
+  waitpid(child1, &status2, WUNTRACED);
+  
+  fprintf(stderr, "child 1 %s: $$ = %d\n", child1);
+  fprintf(stderr, "child 2 %s: $$ = %d\n", child2);
+  
   
 }
