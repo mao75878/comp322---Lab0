d06e8e7209d2e6a410807695dc03d84931c8f7d6
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-05 12:29:25 -0700

    Update catcher.c

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include <stdlib.h>
+#include <string.h>
 
+static int count = 0;
+static int term = 0;
+
+int pid = getpid();
+fprintf(stderr, "catcher: $$ = %d\n", pid); //The program includes a status line that includes its PID to stderr
