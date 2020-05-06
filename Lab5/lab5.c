a9cc7841b1d12a4ee2282dd37c55a483ed40aeaa
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-06 14:43:16 -0700

    Update lab5.c

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv){
-const int pSize = 4096;
-unsigned int address, pNum, offset;
+void getpNum(unsigned int address, int pSize)
+{
+  unsigned int pNum;
+  pNum = address/ pSize;
+  printf("page number = %d\n", pNum)
 }
+
+getOffset(unsigned int address, int pSize){
+  unsigned int offset;
+  offset = address % pSize;
+  printf("offset = %d\n", offset);
+  
+  
+int main(int argc, char **argv){
+
+const int pSize = 4096;
+unsigned int address;
+    
+if(argc == 2){
+  address = atoi(argv[1]);
+  printf("The address %d contains:"\n);
+  getpNum(address, pSize);
+  getOffset(address, pSize);
+  
+}
+  
+ 
