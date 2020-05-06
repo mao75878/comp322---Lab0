8b13ec411bf938d6d06b87bdfccb93d36afbe3b2
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-06 14:56:58 -0700

    Update lab5.c

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -6,15 +6,15 @@ void getpNum(unsigned int address, int pSize)
 {
   unsigned int pNum;
   pNum = address/ pSize;
-  printf("page number = %d\n", pNum)
+  printf("page number = %d\n", pNum);
 }
 
 //computes offset and prints it
-getOffset(unsigned int address, int pSize){ 
+void getOffset(unsigned int address, int pSize){ 
   unsigned int offset;
   offset = address % pSize;
   printf("offset = %d\n", offset);
-  
+}  
   
 int main(int argc, char **argv){
 
@@ -23,11 +23,11 @@ unsigned int address;
     
 if(argc == 2){
   address = atoi(argv[1]); //convert argv[1] to int for address
-  printf("The address %d contains:"\n, address); //prints address
+  printf("The address %d contains:\n", address); //prints address
   getpNum(address, pSize); //explained above
   getOffset(address, pSize); //explained above
-  
+
   return 0;
 }
-  
+}  
  
