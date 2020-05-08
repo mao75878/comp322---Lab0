716a1acf8cab8c7f39d846f66bd1444899216fed
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-07 21:05:05 -0700

    Update dining-p.c

--- Lab4/dining-p.c
+++ Lab4/dining-p.c
@@ -9,3 +9,16 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <sys/wait.h>
+
+
+void eat(int n){
+  fprintf(stdout,"Philosopher #%d is eating\n", n);
+  usleep(rand() % 1000000);
+}
+
+void think(int n){
+  fprintf(stdout, "Philosopher #%d is thinking\n", n);
+  usleep(rand() % 1000000);
+}
+
+  
