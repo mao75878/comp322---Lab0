0d7adc9d1cf623ae6d34bef1b7fb1087178466e0
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-07 23:51:40 -0700

    Update mole.c

--- Lab6/mole.c
+++ Lab6/mole.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *[]){
+  FILE *logfile = fopen("lab6.log", "a");
+  fprintf(logfile, "Pop %s\n", argv[0]);
+  fclose(logfile);
+  return 0;
+}
