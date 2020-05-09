a237f7b947985c0af3b5ae7af35e0ddc196fb29d
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-08 21:01:57 -0700

    Update dining-p.c

--- Lab4/dining-p.c
+++ Lab4/dining-p.c
@@ -13,7 +13,6 @@
 #define CS1 "/chopstick1"
 #define CS2 "/chopstick2"
 
-static int finish;
 sem_t* stick1;
 sem_t* stick2;
 static int finish;
@@ -34,9 +33,9 @@ void sigHandler(int sig)
   finish = 1;
 }
 
-void killSem()
+void killSem() //Develop a handler for SIGTERM
 {
-  sem_close(stick1);
+  sem_close(stick1); //Program needs to effectively release any system resources
   sem_close(stick2);
   
   sem_unlink(CS1);
@@ -66,28 +65,30 @@ int main(int argc, char** argv)
   {
     int cyCount = 0;
     signal(SIGTERM, sigHandler);
-    stick1 = sem_open(CS1, O_CREAT, 0660, 1);
-    stick2 = sem_open(CS2, O_CREAT, 0660, 1);
+    
+    //Add semaphores to the program
+    stick1 = sem_open(CS1, O_CREAT, 0660, 1); //allocate a semaphore for each chopstick
+    stick2 = sem_open(CS2, O_CREAT, 0660, 1); 
     
     do
     {
-      sem_wait(stick1);
+      sem_wait(stick1); //modify the wait() function to wait on the semaphore
       sem_wait(stick2);
       
-      eat(phil);
+      eat(phil);       //Augment the code to use the eat function
       
-      sem_post(stick1);
+      sem_post(stick1); //modify the signal() function to post to the semaphore
       sem_post(stick2);
       
-      think(phil);
+      think(phil);     //Augment the code to use the think function
       
-      cyCount++;
+      cyCount++; //Augment the code to keep a count of the number of full eat-think cycles
     }
     
   while(finish);
   if(finish != 1)
   {
-    fprintf(stderr, "Philospher #%d completed %d cycles.\n", phil, cyCount);
+    fprintf(stderr, "Philospher #%d completed %d cycles.\n", phil, cyCount); //program needs to emit a line to stderr the following line
   }
     
    killSem();
