a0c56428d35ba2f0ccdead7d7190d875501bacc6
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-08 18:44:20 -0700

    Update dining-p.c

--- Lab4/dining-p.c
+++ Lab4/dining-p.c
@@ -10,6 +10,12 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+#define CS1 "/chopstick1"
+#define CS2 "/chopstick2"
+
+static int finish;
+sem_t* side1;
+sem_t* side2;
 
 void eat(int n){
   fprintf(stdout,"Philosopher #%d is eating\n", n);
@@ -21,20 +27,74 @@ void think(int n){
   usleep(rand() % 1000000);
 }
 
-int main(int argc, char** argv){
-int philNum =
-  
-  do{
-    wait(chopstick[i]);
-    wait(chopstick[(i+1)%5]);
-    
-    eat();
-    
-    signal(chopstick[i]);
-    signal(chopstick[(i+1)];
-           
-    think(); 
-    }
+int cycles
+
+void sigHandler(int sig)
+{
+  signal(SIGTERM, sigHandler);
+  finish = 1;
 }
-           
+
+void killSem()
+{
+  sem_close(side1);
+  sem_close(side2);
+  
+  sem_unlink(CS1);
+  sem_unlink(CS2);
+  
+  sem_destroy(side1);
+  sem_destroy(side2);
+}
+
+
+int main(int argc, char** argv){
+ int seats = atoi(argv[1]);
+ int phil = atoi(argv[2]);
+ 
+  if(argc < 3)
+ {
+   printf("Not enough args\n");
+    exit(EXIT_FAILURE);
+ }
+  
+  if(phil > seats)
+  {
+    printf("Not enough seats");
+  }
+  else if(argc == 3)
+  {
+    int cyCount = 0;
+    signal(SIGTERM, sighandler);
+    side1 = sem_open(CS1, O_CREAT, 0660, 1);
+    side2 = sem_open(CS2, O_CREAT, 0660, 1);
+    
+    do
+    {
+      sem_wait(side1);
+      sem_wait(side2);
+      
+      eat(position);
+      
+      sem_post(side1);
+      sem_post(side2);
+      
+      think(position);
+      
+      cyCount++;
+    }
+    
+  while(finish);
+  if(finish != 1)
+  {
+    fprintf(stderr, "Philospher #%d completed %d cycles.\n", position, cycles);
+  }
+    
+   killSem();
+  }
+}
+
+
+exit(EXIT_SUCCESS);
+}
     
