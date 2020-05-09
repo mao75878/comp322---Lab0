5d7003a11bd838cb1b79ef6103fbe030a4b6a8df
mao75878 <61486199+mao75878@users.noreply.github.com>
2020-05-08 20:00:05 -0700

    Update launch-philosophers.sh

--- Lab4/launch-philosophers.sh
+++ Lab4/launch-philosophers.sh
@@ -1,1 +1,33 @@
 
+
+EXEC=dining-p
+SEATS=$1
+child=$!
+pids=()
+
+
+sig_handler(){
+
+	for((i=0; $i<$SEATS; i+=1))
+	do
+		kill -TERM ${pids[i]} 2>/dev/null
+	done
+
+
+	exit 0
+}
+
+
+trap 'sig_handler' TERM
+
+
+for ((i=0; $i<$SEATS; i+=1))
+do
+
+
+	./$EXEC $SEATS $i &
+
+
+	pids[${i}]=$! #pid of children
+done
+wait
